03f144657de01a52805b40d546dce532d1bd7b81
Nicholas Shankland <nicholas.m.shankland@gmail.com>
2023-03-22 18:29:11 -0700

    generatinga  random number

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,7 +1,10 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
+
+    let rand_num: i32 = rand::thread_rng().gen_range(1..=100);
 
     println!("Please input your guess.");
 
